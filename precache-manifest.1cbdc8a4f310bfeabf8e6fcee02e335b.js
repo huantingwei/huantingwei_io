aff8f85bd4bfc3587f03fc41196db6a98384952c
huantingwei <ting02940@gmail.com>
2020-10-23 16:12:28 +0800

    Updates

---
+++ precache-manifest.1cbdc8a4f310bfeabf8e6fcee02e335b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42081c69c421bc2945404e30c2befee2",
+    "url": "/huantingwei_io/index.html"
+  },
+  {
+    "revision": "6259966b82c984e9a84f",
+    "url": "/huantingwei_io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "22fb426f39d4060f1b46",
+    "url": "/huantingwei_io/static/js/2.b9cf9757.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/huantingwei_io/static/js/2.b9cf9757.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6259966b82c984e9a84f",
+    "url": "/huantingwei_io/static/js/main.6fa1402b.chunk.js"
+  },
+  {
+    "revision": "ecd9fd4c34f544fe8a6e",
+    "url": "/huantingwei_io/static/js/runtime-main.bb4ffb89.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/huantingwei_io/static/media/logo.5d5d9eef.svg"
+  }
+]);
